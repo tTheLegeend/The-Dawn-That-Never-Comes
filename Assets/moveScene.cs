14c40732acd0b2d5979529f437ee6e7e0979a812
TheLegeend <kyubi.naruto99@yahoo.com>
2021-02-09 16:17:51 +0000

    MoveLevel

---
+++ Assets/moveScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class nextScene : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+    void OnMouseUp()
+    {
+        SceneManager.LoadScene(newLevel);
+    }
+}
