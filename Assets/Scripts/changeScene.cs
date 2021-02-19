a76fb539434c7f5721eaed3f4c7d91e68dd8ed99
tTheLegeend <78236265+tTheLegeend@users.noreply.github.com>
2021-02-19 18:12:58 +0000

    Menu Button Scripts & 4 Scenes and an asset (#1)
    * MoveLevel
    
    * Exit button script
    
    Exit script
    
    * Exit & ChangeScene
    
    Exit Script
    Change Scene
    all 4 scenes
    black asset
    
    Co-authored-by: TheLegeend <kyubi.naruto99@yahoo.com>

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class changeScene : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+    public void OnMouseUp()
+    {
+        SceneManager.LoadScene(newLevel);
+        Debug.Log("Next Scene");
+    }
+}
