4e12446a1516dca49b20a1ee923cfb533366819f
r3sp3ctHsM <r3sp3ctHsM@users.noreply.github.com>
2021-03-25 20:40:26 +0000

    reverting cos i think i messed up

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    //public Animator myAnim;
-    public Transform target;
+    private Animator myAnim;
+    private Transform target;
     public Transform homePos;
     public GameObject die;
-    public Rigidbody2D rb;
-    public Transform look;
 
     [SerializeField]
     private float speed;
@@ -17,26 +15,15 @@ public class EnemyAI : MonoBehaviour
     private float maxRange;
     [SerializeField]
     private float minRange;
-    [SerializeField]
-    private float attackRange;
 
     private int health = 100;
-
-    public Transform firePoint;
-
-    public Vector2 targetPosV2;
-
-    public float attackRate = 2f;
-    private float nextAttackTime = 0f;
-
-
-    void FixedUpdate()
+    private float timePassed;
+    void Start()
     {
-        targetPosV2 = target.position;
-        Vector2 lookDir = targetPosV2 - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
-        look.eulerAngles = new Vector3(0, 0, angle);
+        myAnim = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerMovement>().transform;
     }
+
     void Update()
     {
         if (Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position) >= minRange)
@@ -47,38 +34,25 @@ public class EnemyAI : MonoBehaviour
         {
             GoHome();
         }
-
-        if (Time.time >= nextAttackTime)
-        {
-
-            if (Vector3.Distance(target.position, transform.position) <= attackRange)
-            {
-                
-                target.GetComponent<Shooting>().Melee(firePoint.position, firePoint.rotation, firePoint);
-                nextAttackTime = Time.time + 1f / attackRate;
-            }
-
-        }
     }
 
     public void followPlayer()
     {
-        //myAnim.SetBool("isMoving", true);
-        //myAnim.SetFloat("MoveX", (target.position.x - transform.position.x));
-        //myAnim.SetFloat("MoveY", (target.position.y - transform.position.y));
+        myAnim.SetBool("isMoving", true);
+        myAnim.SetFloat("MoveX", (target.position.x - transform.position.x));
+        myAnim.SetFloat("MoveY", (target.position.y - transform.position.y));
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-     
     }
 
     public void GoHome()
     {
-        //myAnim.SetFloat("MoveX", (homePos.position.x - transform.position.x));
-        //myAnim.SetFloat("MoveY", (homePos.position.y - transform.position.y));
+        myAnim.SetFloat("MoveX", (homePos.position.x - transform.position.x));
+        myAnim.SetFloat("MoveY", (homePos.position.y - transform.position.y));
         transform.position = Vector3.MoveTowards(transform.position, homePos.position, speed * Time.deltaTime);
 
         if(Vector3.Distance(transform.position, homePos.position) == 0)
         {
-            //myAnim.SetBool("isMoving", false);
+            myAnim.SetBool("isMoving", false);
         }
 
         
@@ -88,28 +62,26 @@ public class EnemyAI : MonoBehaviour
     {
         if (health > dmg)
         {
-            //myAnim.SetTrigger("isHurt");
+            myAnim.SetTrigger("isHurt");
             health = health - dmg;
             Debug.Log("ouch");
         }
         else
         {
-
-            death();
-
+            
+                death();
+            
         }
     }
 
     public void death()
     {
+        myAnim.SetBool("isDead", true);
         Destroy(gameObject);
-
-        //myAnim.SetBool("isDead", true);
-
-        //GameObject death = Instantiate(die, transform.position, Quaternion.identity);
-        //Destroy(death, .5f);
+        GameObject death = Instantiate(die, transform.position, Quaternion.identity);
+        Destroy(death, .5f);
         Debug.Log("death");
-
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -120,8 +92,6 @@ public class EnemyAI : MonoBehaviour
         }
 
     }
-
-
 
 
 }
