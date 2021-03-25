e5e8bdf320f54c221e3acc70943d636b9c010023
TheLegeend <kyubi.naruto99@yahoo.com>
2021-03-25 21:08:21 +0000

    Revert "Merge branch 'main' into TheLegend"
    This reverts commit d2492c5f8f27b226ed3b26a84036964480f5a885, reversing
    changes made to a65417359abbedf51290a176f1bb095e351e9bc8.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    private Animator myAnim;
-    private Transform target;
+    //public Animator myAnim;
+    public Transform target;
     public Transform homePos;
     public GameObject die;
+    public Rigidbody2D rb;
+    public Transform look;
 
     [SerializeField]
     private float speed;
@@ -15,15 +17,26 @@ public class EnemyAI : MonoBehaviour
     private float maxRange;
     [SerializeField]
     private float minRange;
+    [SerializeField]
+    private float attackRange;
 
     private int health = 100;
-    private float timePassed;
-    void Start()
-    {
-        myAnim = GetComponent<Animator>();
-        target = FindObjectOfType<PlayerMovement>().transform;
-    }
 
+    public Transform firePoint;
+
+    public Vector2 targetPosV2;
+
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
+
+
+    void FixedUpdate()
+    {
+        targetPosV2 = target.position;
+        Vector2 lookDir = targetPosV2 - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
+        look.eulerAngles = new Vector3(0, 0, angle);
+    }
     void Update()
     {
         if (Vector3.Distance(target.position, transform.position) <= maxRange && Vector3.Distance(target.position, transform.position) >= minRange)
@@ -34,25 +47,38 @@ public class EnemyAI : MonoBehaviour
         {
             GoHome();
         }
+
+        if (Time.time >= nextAttackTime)
+        {
+
+            if (Vector3.Distance(target.position, transform.position) <= attackRange)
+            {
+                
+                target.GetComponent<Shooting>().Melee(firePoint.position, firePoint.rotation, firePoint);
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+
+        }
     }
 
     public void followPlayer()
     {
-        myAnim.SetBool("isMoving", true);
-        myAnim.SetFloat("MoveX", (target.position.x - transform.position.x));
-        myAnim.SetFloat("MoveY", (target.position.y - transform.position.y));
+        //myAnim.SetBool("isMoving", true);
+        //myAnim.SetFloat("MoveX", (target.position.x - transform.position.x));
+        //myAnim.SetFloat("MoveY", (target.position.y - transform.position.y));
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+     
     }
 
     public void GoHome()
     {
-        myAnim.SetFloat("MoveX", (homePos.position.x - transform.position.x));
-        myAnim.SetFloat("MoveY", (homePos.position.y - transform.position.y));
+        //myAnim.SetFloat("MoveX", (homePos.position.x - transform.position.x));
+        //myAnim.SetFloat("MoveY", (homePos.position.y - transform.position.y));
         transform.position = Vector3.MoveTowards(transform.position, homePos.position, speed * Time.deltaTime);
 
         if(Vector3.Distance(transform.position, homePos.position) == 0)
         {
-            myAnim.SetBool("isMoving", false);
+            //myAnim.SetBool("isMoving", false);
         }
 
         
@@ -62,26 +88,28 @@ public class EnemyAI : MonoBehaviour
     {
         if (health > dmg)
         {
-            myAnim.SetTrigger("isHurt");
+            //myAnim.SetTrigger("isHurt");
             health = health - dmg;
             Debug.Log("ouch");
         }
         else
         {
-            
-                death();
-            
+
+            death();
+
         }
     }
 
     public void death()
     {
-        myAnim.SetBool("isDead", true);
         Destroy(gameObject);
-        GameObject death = Instantiate(die, transform.position, Quaternion.identity);
-        Destroy(death, .5f);
+
+        //myAnim.SetBool("isDead", true);
+
+        //GameObject death = Instantiate(die, transform.position, Quaternion.identity);
+        //Destroy(death, .5f);
         Debug.Log("death");
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -92,6 +120,8 @@ public class EnemyAI : MonoBehaviour
         }
 
     }
+
+
 
 
 }
---
+++ Assets/Scripts/Inventory/OpenClose.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class OpenClose : MonoBehaviour
+{
+    [SerializeField] GameObject CanvasInventory;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        CanvasInventory = GameObject.Find("CanvasInventory");
+        CanvasInventory.SetActive(false);
+        //UnityEngine.Debug.Log("Inventory Inactive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (CanvasInventory.activeSelf)
+        {
+            if (Input.GetKeyDown("i"))
+            {
+                CanvasInventory.SetActive(false);
+                //UnityEngine.Debug.Log("Inventory Inactive");
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown("i"))
+            {
+                CanvasInventory.SetActive(true);
+               //UnityEngine.Debug.Log("Inventory Active");
+            }
+        }
+
+
+
+}
+
+    
+}
