6bba10960288ac79db02aa99a31ce2744d09549e
TheLegeend <kyubi.naruto99@yahoo.com>
2021-03-25 18:35:17 +0000

    Inventory Completed

--- Assets/Scripts/Inventory/OpenClose.cs
+++ Assets/Scripts/Inventory/OpenClose.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class OpenClose : MonoBehaviour
 {
+    GameObject CanvasInventory;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        CanvasInventory = GameObject.Find("CanvasInventory");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (CanvasInventory.activeSelf)
+        {
+            if (Input.GetKeyDown("i"))
+            {
+                CanvasInventory.SetActive(false);
+                UnityEngine.Debug.Log("Inventory Active");
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown("i"))
+            {
+                CanvasInventory.SetActive(true);
+                UnityEngine.Debug.Log("Inventory Not Active");
+            }
+        }
+
+
+
+}
+
+    
 }
