6bddcbec2278bb05e12266a082e9f9e6fe6ed071
TheLegeend <kyubi.naruto99@yahoo.com>
2021-03-25 21:14:30 +0000

    Revert "Revert "Merge branch 'main' into TheLegend""
    This reverts commit e5e8bdf320f54c221e3acc70943d636b9c010023.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,69 +7,25 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-    //public Animator animator;
-
-    // Needed For Inventory
-    public Transform hand; 
-    public Transform body;
-    //~~~~~~~~~~~~~~~~~~~~
+    public Animator animator;
 
     Vector2 movement;
-
-    private int health = 100;
-
+    
     void Update()
     {
         //input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        //animator.SetFloat("Horizontal", movement.x);
-        //animator.SetFloat("Vertical", movement.y);
-        //animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
         //movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-    }
-
-    public void setHealth(int dmg)
-    {
-        if (health > dmg)
-        {
-            //myAnim.SetTrigger("isHurt");
-            health = health - dmg;
-            Debug.Log("ouch");
-        }
-        else
-        {
-
-            death();
-
-        }
-    }
-
-    public void death()
-    {
-        Destroy(gameObject);
-
-        //myAnim.SetBool("isDead", true);
-
-        //GameObject death = Instantiate(die, transform.position, Quaternion.identity);
-        //Destroy(death, .5f);
-        Debug.Log("death");
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Bullet"))
-        {
-            setHealth(20);
-        }
 
     }
 }
