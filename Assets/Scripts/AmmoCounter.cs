efc4d2fa9cb23e034d37e5e3786c5e97fe18395a
raimondsp20 <63418506+raimondsp20@users.noreply.github.com>
2021-04-22 15:02:28 +0100

    Main Menu and ammo counter

---
+++ Assets/Scripts/AmmoCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoCounter : MonoBehaviour
+{
+    public int ammo;
+    public bool isFiring;
+    public Text ammoDisplay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammoDisplay.text = ammo.ToString();
+        if (Input.GetMouseButtonDown(0) && !isFiring && ammo > 0)
+        {
+            isFiring = true;
+            ammo--;
+            isFiring = false; //If ammo has ran out
+        }
+    }
+}
