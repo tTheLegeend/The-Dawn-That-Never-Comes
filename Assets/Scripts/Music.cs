6f4e3c6e3dbbe2ea6f361d0918bb8db771f6b6b3
r3sp3ctHsM <r3sp3ctHsM@users.noreply.github.com>
2021-04-22 15:34:01 +0100

    added simple music prefab
    to use:
    song number is public so it can be changed easily by other prefabs/scripts
    1 - battle music
    2 - calm music
    3 - night music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip battle;
+    public AudioClip calmMusic;
+    public AudioClip nightTime;
+    public int songNumber;
+    private int temp;
+    AudioSource audioData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioData.isPlaying || temp != songNumber)
+        {
+            switch (songNumber)
+            {
+                case 1:
+                    audioData.PlayOneShot(battle, 0.7f);
+                    temp = 1;
+                    break;
+                case 2:
+                    audioData.PlayOneShot(calmMusic, 0.7f);
+                    temp = 2;
+                    break;
+                case 3:
+                    audioData.PlayOneShot(nightTime, 0.7f);
+                    temp = 3;
+                    break;
+                default:
+                    audioData.Stop();
+                    break;
+            }
+        }
+    }
+}
