ee6e470abc87d08028807ea079042bb1f222c99b
TheLegeend <kyubi.naruto99@yahoo.com>
2021-04-28 21:13:01 +0100

    Tryint to fix stuff 2

--- Assets/Scripts/changeSceneCollision.cs
+++ Assets/Scripts/changeSceneCollision.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class changeSceneCollision : MonoBehaviour
 {
-    [SerializeField] private string newLevel;
     bool bPressedF = false;
+
+    [Tooltip("The Transform to teleport to")]
+    [SerializeField] Transform teleportTo;
+
+    [Tooltip("The filter Tag")]
+    [SerializeField] string tag = "Player";
 
     void Update()
     {
@@ -21,8 +26,44 @@ public class changeSceneCollision : MonoBehaviour
     {
         if (other.name == "Player" && bPressedF == true)
         {
-            SceneManager.LoadScene(newLevel);
+            if (tag == string.Empty || other.CompareTag(tag))
+
+            {
+                other.transform.position = teleportTo.position;
+            }
+                
             Debug.Log("Move Scene");
         }
     }
+}
+
+public class TeleportTrigger : MonoBehaviour
+{
+    public enum TriggerType 
+    { 
+        Enter, Exit 
+    };
+
+
+
+    //[Tooltip("Trigger Event to Teleport")]
+    //[SerializeField] TriggerType type;
+
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (type != TriggerType.Enter)
+    //        return;
+
+    //    if (tag == string.Empty || other.CompareTag(tag))
+    //        other.transform.position = teleportTo.position;
+    //}
+
+    //void OnTriggerExit2D(Collider2D other)
+    //{
+    //    if (type != TriggerType.Exit)
+    //        return;
+
+    //    if (tag == string.Empty || other.CompareTag(tag))
+    //        other.transform.position = teleportTo.position;
+    //}
 }
