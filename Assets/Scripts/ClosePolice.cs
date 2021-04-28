72237e80030d3f91552493ca4591c4db96cb382a
TheLegeend <kyubi.naruto99@yahoo.com>
2021-04-28 23:56:38 +0100

    Teleport Fixes

---
+++ Assets/Scripts/ClosePolice.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosePolice : MonoBehaviour
+{
+    public GameObject doorOpen;
+       
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorOpen.SetActive(false);
+    }
+
+}
